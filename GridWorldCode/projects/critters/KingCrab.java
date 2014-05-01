96e5e593fb776eca331ca4813c4a830968fc196e
dazigemm <dazigemm@gmail.com>
2014-04-30 23:09:15 -0400

    skel

---
+++ GridWorldCode/projects/critters/KingCrab.java
@@ -0,0 +1,9 @@
+import info.gridworld.actor.CrabCritter;
+
+public class KingCrab extends CrabCritter {
+    /*
+      causes each actor it processes to move 1 location further away from it
+      If the actor cannot move away, the KingCrab removes it from the grid.
+      When it has completed processing the actors, it moves like a CrabCritter
+    */
+}
---
+++ GridWorldCode/projects/critters/QuickCrab.java
@@ -0,0 +1,11 @@
+import info.gridworld.actor.CrabCritter;
+import java.util.ArrayList;
+
+public class QuickCrab extends CrabCritter {
+
+    /*moves to one of the 2 locations, randomly selected, taht are two spaces to its right or left, if that location & the intervening location are both empty.
+      Otherwise, a QuickCrab moves like a CrabCritter */
+
+    public ArrayList<Actors> getMoveLocations() {
+    }
+}
