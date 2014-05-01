3deb516c5b139fa5021cfd7c0818a68a6a7fb0fc
dazigemm <dazigemm@gmail.com>
2014-05-01 02:03:05 -0400

    QuickCrab

--- GridWorldCode/projects/critters/QuickCrab.java
+++ GridWorldCode/projects/critters/QuickCrab.java
@@ -1,11 +1,23 @@
-import info.gridworld.actor.CrabCritter;
+import info.gridworld.actor.Critter;
+import info.gridworld.grid.Location;
+import info.gridworld.grid.Grid;
 import java.util.ArrayList;
 
 public class QuickCrab extends CrabCritter {
 
-    /*moves to one of the 2 locations, randomly selected, taht are two spaces to its right or left, if that location & the intervening location are both empty.
+    /*moves to one of the 2 locations, randomly selected, that are two spaces to its right or left, if that location & the intervening location are both empty.
       Otherwise, a QuickCrab moves like a CrabCritter */
 
-    public ArrayList<Actors> getMoveLocations() {
+    public ArrayList<Location> getLocationsInDirections(int[] directions) {
+	ArrayList<Location> locs = new ArrayList<Location>();
+	Grid gr = getGrid();
+	Location loc = getLocation();
+	for (int d : directions) {
+	    int dir = getDirection() + d;
+	    Location neighborLoc = loc.getAdjacentLocation(dir);
+	    if (gr.isValid(neighborLoc) && gr.get(neighborLoc) == null)
+		locs.add(neighborLoc.getAdjacentLocation(dir));
+	}
+	return locs;
     }
 }
