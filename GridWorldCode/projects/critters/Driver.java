ce2a2072ef83ef98f7633f96d8df101989d7fcde
dazigemm <dazigemm@gmail.com>
2014-05-01 02:11:45 -0400

    added driver class to use for testing

---
+++ GridWorldCode/projects/critters/Driver.java
@@ -0,0 +1,15 @@
+//use this class to test?
+import info.gridworld.actor.ActorWorld;
+import info.gridworld.actor.Bug;
+import info.gridworld.actor.Flower;
+import info.gridworld.actor.Rock;
+import info.gridworld.grid.Location;
+
+public class Driver {
+    public static void main (String[] args) {
+	ActorWorld world = new ActorWorld();
+	world.add(new Location(3,5), new QuickCrab());
+	world.add(new Location(3,6), new Rock());
+	world.show();
+    }
+}
