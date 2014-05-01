a1859ab81e75d56dbbdf19b2371bad0819aef674
dazigemm <dazigemm@gmail.com>
2014-04-30 22:52:15 -0400

    added import statements to BlusterCritter

--- GridWorldCode/projects/critters/BlusterCritter.java
+++ GridWorldCode/projects/critters/BlusterCritter.java
@@ -1,11 +1,18 @@
-public class BlusterCritter extends Critter{
+import info.gridworld.actor.Critter;
+import info.gridworld.actor.Actor;
+import java.util.ArrayList;
+import info.gridworld.grid.Location;
+
+public class BlusterCritter extends Critter {
     private int c;
 
-    public BlusterCritter{
+    public BlusterCritter (){
+	//super();
         c = 5;
     }
 
     public ArrayList<Actor> getActors(){
-        return getGrid().getNeighbors(getLocation());//We have to somehow modify this to get all neighbors within two boxes.
+        return getGrid().getNeighbors(getLocation());
+	//We have to somehow modify this to get all neighbors within two boxes.
     } 
 }
