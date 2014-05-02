b2dcab2bac2351def090ae4152564c3ee6ef86c5
dazigemm <dazigemm@gmail.com>
2014-05-02 18:17:42 -0400

    import statements

--- GridWorldCode/projects/critters/RockHound.java
+++ GridWorldCode/projects/critters/RockHound.java
@@ -1,7 +1,8 @@
 import info.gridworld.actor.Critter;
 import info.gridworld.actor.Rock;
-
-public class RockHounds extends Critter{
+import info.gridworld.actor.Actor;
+import java.util.ArrayList;
+public class RockHound extends Critter{
     public void processActors(ArrayList<Actor> actors){
         for (Actor a : actors){
             if (a instanceof Rock){
